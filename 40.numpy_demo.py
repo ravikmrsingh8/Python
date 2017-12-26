db202ad2347ccd719695626999dd53a62060fcbd
Ravi Kumar Singh <RaviKumar.Singh@jda.com>
2017-12-27 00:18:48 +0530

    sin(x) plot

--- 40.numpy_demo.py
+++ 40.numpy_demo.py
@@ -1,26 +1,23 @@
-import numpy as np
-
 from numpy import random
-
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
-def sinplot(flip=1):
+def sinplot():
     x = np.linspace(0, 14, 100)
-    for i in range(1, 7):
-        plt.plot(x, np.sin(x + i * .5) * (7 - i) * flip)
+    plt.plot(x, np.sin(x))
+    plt.ylabel("sin(x)")
+    plt.xlabel("x")
+    plt.show()
 
 
 def run():
     print(np.zeros(10))
     print(np.arange(10, 21))
-    print(np.arange(0,9).reshape(3,3))
+    print(np.arange(0, 9).reshape(3, 3))
     print(np.identity(3))
     print(random.rand())
-    a = np.arange(0,16).reshape(4,4)
+    a = np.arange(0, 16).reshape(4, 4)
 
     print(a)
     print("")
@@ -29,5 +26,5 @@ def run():
     sinplot()
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     run()
